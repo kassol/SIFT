2e20beae4f41e6ff864ea09e05f68f9f90721eed
kassol <kassol.zx@gmail.com>
2013-11-06 13:12:35 +0800

    modify main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	match m("D:\\01-155_50mic.tif", "D:\\01-156_50mic.tif");
+	match m("D:\\02-164_50mic.tif", "D:\\02-165_50mic.tif");
 	std::vector<SamePoint> result;
 	m.domatch(result);
 	system("pause");
